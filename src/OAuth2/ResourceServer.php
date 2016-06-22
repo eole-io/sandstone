51a07bfc458eaad013fe066b52bbc078f940e4a0
Julien Maulny <doubjulien@hotmail.fr>
2016-06-22 19:13:59 +0200

    use interfaces instead of concrete implementation in server constructs, remove unused Tests

--- src/OAuth2/ResourceServer.php
+++ src/OAuth2/ResourceServer.php
@@ -6,23 +6,4 @@ use League\OAuth2\Server\ResourceServer as BaseResourceServer;
 
 class ResourceServer extends BaseResourceServer
 {
-    /**
-     * @param Storage\Session $sessionStorage
-     * @param Storage\AccessToken $accessTokenStorage
-     * @param Storage\Client $clientStorage
-     * @param Storage\Scope $scopeStorage
-     */
-    public function __construct(
-        Storage\Session $sessionStorage,
-        Storage\AccessToken $accessTokenStorage,
-        Storage\Client $clientStorage,
-        Storage\Scope $scopeStorage
-    ) {
-        parent::__construct(
-            $sessionStorage,
-            $accessTokenStorage,
-            $clientStorage,
-            $scopeStorage
-        );
-    }
 }
