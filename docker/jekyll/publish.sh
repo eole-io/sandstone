88e69ae593b08869a95270133eca64b4dc649fec
Alcalyn <julien.maulny@protonmail.com>
2017-09-21 16:12:47 +0000

    Publish

---
+++ docker/jekyll/publish.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+rm -fr /tmp/sandstone /tmp/sandstone-build
+
+bundle install
+bundle exec jekyll build --destination /tmp/sandstone-build/
+cd ..
+
+cd /tmp
+git clone https://github.com/eole-io/sandstone.git
+cd sandstone/
+
+git config --global user.email "julien.maulny@protonmail.com"
+git config --global user.name "Alcalyn"
+
+git fetch origin
+git checkout gh-pages
+rm -fr *
+mv /tmp/sandstone-build/* .
+rm Gemfile Gemfile.lock README.md
+git add -A
+git commit -m "Publish"
+git push origin gh-pages
+cd /
+
+rm -fr /tmp/sandstone /tmp/sandstone-build
